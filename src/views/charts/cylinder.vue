04293ead1fd3f537fc594413bc3c8c56e8aa5e5a
nieyulin <nieyulin@bldz.com>
2019-10-16 09:29:33 +0800

    feat: 圆锥体的处理

--- src/views/charts/cylinder.vue
+++ src/views/charts/cylinder.vue
@@ -13,30 +13,22 @@ export default {
       camera: null,
       scene: null,
       renderer: null,
-      mesh: null
+      mesh: null,
+      cylinder: null,
+      angularSpeed: 0.2,
+      lastTime: 0
     }
   },
   methods: {
     // 初始化图形
     init() {
       let container = document.getElementById('container');
-      this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 10)
-      this.camera.position.z = 1
-      this.camera.position.y = 0.01
+      this.camera = new Three.PerspectiveCamera(45, container.clientWidth/container.clientHeight, 1, 1000)
+      this.camera.position.z = 700
       this.scene = new Three.Scene()
-      // 创建物体
-      let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2)
-      // 创建材质
-      let material = new Three.MeshNormalMaterial()
-      // 创建光源
-      let spotLight = new Three.SpotLight(0xff0000)
-      spotLight.position.set(30, 25, -2)
-      spotLight.castShadow = true
-      // 将图形和材质放入网格中，先放入图形，再放入网格
-      this.mesh = new Three.Mesh(geometry, material)
-      // 将网格放入场景中
-      this.scene.add(this.mesh)
-      this.scene.add(spotLight)
+      this.cylinder = new Three.Mesh(new Three.CylinderGeometry(0, 100, 400, 50, 50, false), new Three.MeshNormalMaterial())
+      this.cylinder.overdraw = true
+      this.scene.add(this.cylinder)
       // 创建web渲染器
       this.renderer = new Three.WebGLRenderer({antialias: true})
       // 设置渲染器的大小
@@ -47,9 +39,11 @@ export default {
     animate() {
       // 使用动画效果，浏览器全新的动画效果
       window.requestAnimationFrame(this.animate)
-      // 设置网格的坐标
-      this.mesh.rotation.x += 0.01
-      this.mesh.rotation.y += 0.02
+      var time = (new Date()).getTime()
+      var timeDiff = time - this.lastTime
+      var angleChange = this.angularSpeed * timeDiff * 2 * Math.PI / 1000
+      this.cylinder.rotation.x += angleChange
+      this.lastTime = time
       // 将场景和图形动态的渲染到渲染器上去
       this.renderer.render(this.scene, this.camera)
     }
--- src/views/charts/index.vue
+++ src/views/charts/index.vue
@@ -3,7 +3,7 @@
     <van-cell-group title="全景超级3d效果">
       <van-cell title="球型动画" is-link to="ballIndex" />
       <van-cell title="正方体动画" is-link to="sequreIndex" />
-      <van-cell title="圆锥体" is-link to="sequreIndex" />
+      <van-cell title="圆锥体" is-link to="cylinder" />
     </van-cell-group>
   </div>
 </template>
